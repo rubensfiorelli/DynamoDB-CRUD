26700a3665cb4f5c35434a0acf7f90d3be04a23b
Rubens Fiorelli <rubensfiorelli@outllok.com>
2024-01-21 14:01:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using DynamoDB_Test.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoDB_Test.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDynamoDBContext _context;
+
+        public ProductsController(IDynamoDBContext context) => _context = context;
+
+        [HttpGet("{id}/{barcode}")]
+        public async Task<IActionResult> Get(string id, string barcode)
+        {
+            var product = await _context.LoadAsync<Product>(id, barcode);
+
+            if(product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _context.ScanAsync<Product>(default).GetRemainingAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product request)
+        {
+            var product = await _context.LoadAsync<Product>(request.Id, request.Barcode);
+
+            if(product is not null) return BadRequest($"Product with ID {request.Id} and Barcode {request.Barcode} exists");
+            await _context.SaveAsync(request);
+
+            return Ok(request);
+
+        }
+
+        [HttpDelete("{id}/{barcode}")]
+        public async Task<IActionResult> Delete(string id, string barcode)
+        {
+            var product = await _context.LoadAsync<Product>(id, barcode);
+
+            if (product is null)
+                return NotFound();
+
+            await _context.DeleteAsync(product);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Product request)
+        {
+            var product = await _context.LoadAsync<Product>(request.Id, request.Barcode);
+
+            if (product is null)
+                return NotFound();
+
+            await _context.SaveAsync(request);
+
+            return Ok(request);
+        }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace DynamoDB_Test.Entities
+{
+    [DynamoDBTable("products")]
+    public class Product
+    {
+        [DynamoDBHashKey("id")]
+        public string Id { get; set; }
+
+        [DynamoDBRangeKey("barcode")]
+        public string Barcode { get; set; }
+
+        [DynamoDBProperty("name")]
+        public string Name { get; set; }
+
+        [DynamoDBProperty("description")]
+        public string Description { get; set; }
+
+        [DynamoDBProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
